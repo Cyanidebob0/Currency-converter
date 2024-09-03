92e97e2b8c39e237868f2d6f7c67882123fda687
bhuvan <bhuvanannappa@gmail.com>
2024-09-03 21:36:51 +0530

    The flags update when the countries are changed

--- app.js
+++ app.js
@@ -8,5 +8,35 @@ for( let select of selects){
          newOption.innerText=currCode;
          newOption.value=currCode;
          select.append(newOption);
+
+         if(select.name==="dropdown-from" && currCode==="USD"){
+            newOption.selected ="selected";
+         }
+         else if(select.name==="dropdown-to" && currCode==="INR"){
+            newOption.selected ="selected";
+         }
+    }
+    select.addEventListener("change",(res)=>{
+        updateFlag(res.target);
+    })
+}
+
+
+
+
+let updateFlag = (element) => {
+    let cCode = element.value;
+    let countryCode = countryList[cCode];
+
+    
+    if (element.name === 'dropdown-from') {
+        let flagFrom = document.querySelector("#flag-from");
+        flagFrom.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    } else if (element.name === 'dropdown-to') {
+        let flagTo = document.querySelector("#flag-to");
+        flagTo.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
     }
 }
+
+
+
